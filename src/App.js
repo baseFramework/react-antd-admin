e57504f3ee7b110c5a5d41fedf03800f972b6b8d
wurh <ro87630872@126.com>
2019-04-15 01:08:05 -0700

    【UPD】完成页面分块
    完成页面分块

--- src/App.js
+++ src/App.js
@@ -2,24 +2,14 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route,Link } from "react-router-dom";
 import Home from './pages/Home/Home'
+import About from './pages/About/About'
+
 
 const App = () => (
   <Router>
   <div>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-      <li>
-        <Link to="/topics">Topics</Link>
-      </li>
-    </ul>
-    <hr />
-
     <Route exact path="/" component={Home} />
+    <Route exact path="/about" component={About} />
   </div>
 </Router>
 );
