4639329e664b1a3b436a7b904e88df74782b50ab
wurh <ro87630872@126.com>
2019-04-18 22:46:19 -0700

    【UPD】更新首页ui
    更新首页ui

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -17,22 +17,40 @@ export default class Home extends Component{
             <div className="homepage">
               <Row gutter={16}>
                 <Col className="gutter-row" span={4}>
-                  <div className="gutter-box">col-6</div>
+                  <div className="gutter-box">
+                    <div>今日销售订单</div>
+                    <div>6</div>
+                  </div>
                 </Col>
                 <Col className="gutter-row" span={4}>
-                  <div className="gutter-box">col-6</div>
+                <div className="gutter-box">
+                    <div>今日销售额</div>
+                    <div>$50</div>
+                  </div>
                 </Col>
                 <Col className="gutter-row" span={4}>
-                  <div className="gutter-box">col-6</div>
+                <div className="gutter-box">
+                    <div>今日发放佣金</div>
+                    <div>$60.77</div>
+                  </div>
                 </Col>
                 <Col className="gutter-row" span={4}>
-                  <div className="gutter-box">col-6</div>
+                <div className="gutter-box">
+                    <div>今日开单店主</div>
+                    <div>6</div>
+                  </div>
                 </Col>
                 <Col className="gutter-row" span={4}>
-                  <div className="gutter-box">col-6</div>
+                <div className="gutter-box">
+                    <div>今日下单店主</div>
+                    <div>16</div>
+                  </div>
                 </Col>
                 <Col className="gutter-row" span={4}>
-                  <div className="gutter-box">col-6</div>
+                <div className="gutter-box">
+                    <div>今日自购店主</div>
+                    <div>19</div>
+                  </div>
                 </Col>
               </Row>
             </div>
