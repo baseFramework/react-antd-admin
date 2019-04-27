7644dbeeab71ebc6d804ec974714d307a383c42c
wurh <ro87630872@126.com>
2019-04-27 22:13:19 +0800

    【UPD】更新运营模块
    更新运营模块

--- src/pages/Operate/Operate.js
+++ src/pages/Operate/Operate.js
@@ -4,7 +4,7 @@ import {
   } from 'antd';
   import './Operate.css';
 
-  
+
   const { SubMenu } = Menu;
   const { Header, Content, Sider } = Layout;
 
@@ -20,25 +20,16 @@ export default class Operate extends Component{
           mode="inline"
           defaultSelectedKeys={['1']}
           defaultOpenKeys={['sub1']}
+          inlineCollapsed={false}
           style={{ height: '100%', borderRight: 0 }}
         >
-          <SubMenu key="sub1" title={<span><Icon type="user" />subnav 1</span>}>
-            <Menu.Item key="1">option1</Menu.Item>
-            <Menu.Item key="2">option2</Menu.Item>
-            <Menu.Item key="3">option3</Menu.Item>
-            <Menu.Item key="4">option4</Menu.Item>
+          <SubMenu key="sub1" title={<span><Icon type="user" />商品管理</span>}>
+            <Menu.Item key="1">SPU商品列表</Menu.Item>
           </SubMenu>
-          <SubMenu key="sub2" title={<span><Icon type="laptop" />subnav 2</span>}>
-            <Menu.Item key="5">option5</Menu.Item>
-            <Menu.Item key="6">option6</Menu.Item>
-            <Menu.Item key="7">option7</Menu.Item>
-            <Menu.Item key="8">option8</Menu.Item>
-          </SubMenu>
-          <SubMenu key="sub3" title={<span><Icon type="notification" />subnav 3</span>}>
-            <Menu.Item key="9">option9</Menu.Item>
-            <Menu.Item key="10">option10</Menu.Item>
-            <Menu.Item key="11">option11</Menu.Item>
-            <Menu.Item key="12">option12</Menu.Item>
+          <SubMenu key="sub2" title={<span><Icon type="laptop" />内容管理</span>}>
+            <Menu.Item key="2">单品专场推荐</Menu.Item>
+            <Menu.Item key="3">主题专场管理</Menu.Item>
+            <Menu.Item key="4">广告管理</Menu.Item>
           </SubMenu>
         </Menu>
       </Sider>
